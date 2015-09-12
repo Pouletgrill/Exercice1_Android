9034e452e12587799eabf0b45beab9ab337134ca
Brosseau <xavierb.2006@hotmail.com>
2015-09-12 18:01:00 -0400

    Ze magic button fini !

--- ZeMagicButton/app/src/main/java/com/example/utilisateur/zemagicbutton/ZeMagicButton.java
+++ ZeMagicButton/app/src/main/java/com/example/utilisateur/zemagicbutton/ZeMagicButton.java
@@ -1,7 +1,9 @@
 package com.example.utilisateur.zemagicbutton;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
@@ -9,9 +11,18 @@ import android.view.View.OnClickListener;
 import android.view.View;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+
 import android.widget.Toast;
 
 public class ZeMagicButton extends AppCompatActivity {
+
+    public List<Button> list_Bouton = new ArrayList<Button>();
+    private int LastPigeBTN=0;
+    private int LastPigeCLR=0;
+    private Random rand = new Random();
+    public int numLeft = 10;
+    public int Niveau = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +30,7 @@ public class ZeMagicButton extends AppCompatActivity {
         setContentView(R.layout.activity_ze_magic_button);
 
         // récupération des widgets depuis le fichier XML
-        List<Button> list_Bouton = new ArrayList<Button>();
+
         //Row 0
         list_Bouton.add((Button) findViewById(R.id.bouton00));
         list_Bouton.add((Button) findViewById(R.id.bouton01));
@@ -45,7 +56,6 @@ public class ZeMagicButton extends AppCompatActivity {
         {
             list_Bouton.get(i).setOnClickListener(new Jouer(list_Bouton.get(i)));
         }
-        list_Bouton.get(0).setOnClickListener(new Jouer(list_Bouton.get(0)));
     }
 
     // la classe interne nommée est privée
@@ -54,39 +64,70 @@ public class ZeMagicButton extends AppCompatActivity {
 
         public Jouer(Button BTN) {
             //this.couleur = couleur;
-            //Toast.makeText(getApplicationContext(),"public Jouer(Button BTN)",
-                    //Toast.LENGTH_SHORT).show();
             btn = BTN;
         }
 
         @Override
         public void onClick(View vue) {
             // fait appel à une méthode hors de la classe interne
-            //Toast.makeText(getApplicationContext(),"public void onClick(View vue)",
-                    //Toast.LENGTH_SHORT).show();
             btn.setVisibility(View.INVISIBLE);
+            int nextPopNum = GetRandUnrepeatNumber(0, list_Bouton.size() - 1);
+            if (numLeft<=1)
+            {
+                ResetGame();
+                list_Bouton.get(nextPopNum).setText(String.valueOf(numLeft-1));
+                int color = randColor();
+                for (int i=0;i<list_Bouton.size();i++)
+                {
+                    list_Bouton.get(i).setBackgroundColor(color);
+                }
+                findViewById(R.id.BackgroundLayout).setBackgroundColor(randColor());
+            }
+            else
+                list_Bouton.get(nextPopNum).setText(String.valueOf(numLeft-1));
+            list_Bouton.get(nextPopNum).setVisibility(View.VISIBLE);
+            numLeft -=1 ;
+
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_ze_magic_button, menu);
-        return true;
+    public void Start_Click(View vue)
+    {
+        int hazardNum = GetRandUnrepeatNumber(0, list_Bouton.size() - 1);
+        list_Bouton.get(hazardNum).setText(String.valueOf(numLeft));
+        list_Bouton.get(hazardNum).setVisibility(View.VISIBLE);
+        vue.setVisibility(View.INVISIBLE);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public int GetRandUnrepeatNumber(int min, int max)
+    {
+        int pige;
+        if (min<max)
+        {do
+            {
+                pige = rand.nextInt((max - min) + 1) + min;
+            } while (pige == LastPigeBTN);
+            LastPigeBTN = pige;
         }
+        else
+            pige = -1;
+        return pige;
+    }
 
-        return super.onOptionsItemSelected(item);
+    public int randColor()
+    {
+        int color = Color.argb(GetRandUnrepeatNumber(0,255),
+                GetRandUnrepeatNumber(0,255),
+                GetRandUnrepeatNumber(0,255),
+                GetRandUnrepeatNumber(0,255));
+        return color;
+    }
+
+    public void ResetGame()
+    {
+        Niveau++;
+        Toast.makeText(getApplicationContext(),"Félicitation ! Niveau "+String.valueOf(Niveau),Toast.LENGTH_LONG).show();
+        LastPigeCLR=0;
+        numLeft=10 + (Niveau * 2)+1;
     }
 }
